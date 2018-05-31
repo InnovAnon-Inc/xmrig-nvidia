21436c8d5ef0193b987cdcb82e25ae5f16c93fc6
Kai Killer <laurence.a.maddox@gmail.com>
2018-05-30 22:24:17 -0500

    update

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 set -exo nounset
 
+[ $# -eq 1 ] || exit 1
+
 [ -x build/xmrig-nvidia ] || ./build.sh
 
 sudo nice -n -20 \
 	build/xmrig-nvidia --donate-level=0 \
-	-u 43To46Y9AxNFkY5rsMQaLwbRNaxLZVvc4LJZt7Cx9Dt23frL6aut2uC3PsMiwGY5C5fKLSn6sWyoxRQTK1dhdBpKAX8bsUW \
-	-p work01 -o us-backup.supportxmr.com
+	--config=$1
+
+#	-u 43To46Y9AxNFkY5rsMQaLwbRNaxLZVvc4LJZt7Cx9Dt23frL6aut2uC3PsMiwGY5C5fKLSn6sWyoxRQTK1dhdBpKAX8bsUW \
+#	-p work01 -o us-backup.supportxmr.com
