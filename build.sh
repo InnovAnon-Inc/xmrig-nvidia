e4cf7d41cda6ea9a53a55be09e4032baa2d460f8
Juju Gouldbergstein <laurence.a.maddox@gmail.com>
2018-07-24 12:43:14 -0500

    backup

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ mkdir build
 cd build
 
 ARG=
-ARG+=' -DCMAKE_C_COMPILER=gcc-5 -DCMAKE_CXX_COMPILER=gcc-5'
+#ARG+=' -DCMAKE_C_COMPILER=gcc-5 -DCMAKE_CXX_COMPILER=gcc-5'
+ARG+=' -DCMAKE_C_COMPILER=gcc-6 -DCMAKE_CXX_COMPILER=gcc-6'
 #ARG+=' -DCUDA_ARCH="20;30;50"'
 
 cmake .. $ARG
